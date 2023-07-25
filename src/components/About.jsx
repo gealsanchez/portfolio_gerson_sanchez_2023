060f396ce491da569a0e948d973508eb4934890d
gealsanchez <gealsanchez@gmail.com>
2023-07-25 16:38:42 -0500

    Portfolio updated

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import '../styles/about.css';
+
+const About = () => (
+  <div id="about" style={{ margin: '0% 10%' }}>
+    Hey There, I am Gerson,
+    software developer.
+    Need help to build a product,
+    feature or website?
+    Take a look of my works.
+    If you like what you see and have a project you need coded,
+    do not hesitate and contact me gealsanchez@gmail.com
+  </div>
+);
+
+export default About;
---
+++ src/components/Header.jsx
@@ -0,0 +1,120 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import emailjs from '@emailjs/browser';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import '../styles/header.css';
+
+const Header = () => {
+  const [show, setShow] = useState(false);
+  const [emailValue, setEmailValue] = useState('');
+  const [subjectValue, setSubjectValue] = useState('');
+  const [look, setLook] = useState(false);
+  const [message, setMessage] = useState('');
+
+  const handleGo = () => setLook(false);
+  const handleLook = () => setLook(true);
+
+  const handleReset = () => {
+    setSubjectValue('');
+    setEmailValue('');
+  };
+
+  const handleClose = () => {
+    setShow(false);
+    handleReset();
+  };
+
+  const handleShow = () => setShow(true);
+  const handleClick = () => {
+    const content = { emailValue, subjectValue };
+    emailjs.send('service_b8fov3c', 'template_xkaa9ue', content, 'c3RthNeKMaL_W7Y1S')
+      .then((result) => {
+        let alert = result.text;
+        alert = ' Thanks for your message';
+        setMessage(alert);
+        handleLook();
+      }, (error) => {
+        let alertwo = error.text;
+        alertwo += 'There is an error, please try again';
+        setMessage(alertwo);
+        handleLook();
+      });
+    handleReset();
+    handleClose();
+  };
+
+  return (
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand to="/">Gerson Sanchez</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <NavLink to="/" id="router-link" end>Home</NavLink>
+              <NavLink to="/products" id="router-link">Projects</NavLink>
+              <NavLink to="/" onClick={() => handleShow()} id="router-link">Contact</NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Please leave a comment</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                value={emailValue}
+                onChange={(event) => setEmailValue(event.target.value)}
+                type="email"
+                placeholder="name@example.com"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group
+              value={subjectValue}
+              onChange={(event) => setSubjectValue(event.target.value)}
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Subject</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClick}>
+            Submit
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal show={look} onHide={handleGo}>
+        <Modal.Header closeButton>
+          <Modal.Title>Gerson Sanchez</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {message}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleGo}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default Header;
